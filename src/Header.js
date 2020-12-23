8d1e69a1f18307fbf1750595ad3d287caa3743cc
shifa kaushar <shifa.kaushar57@gmail.com>
2020-12-23 15:33:22 +0530

    git initial cmit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+// Header
+import React from 'react'
+import  './header.css'
+
+//fuctional- component
+function Header() {
+    return (
+        <div  className='header'>
+            
+        </div>
+    )
+}
+
+export default Header
